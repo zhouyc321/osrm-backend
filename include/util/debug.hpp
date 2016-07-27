929341d331180a63a4384eb2a259da93eefd4e8e
Daniel J. Hofmann <daniel@trvx.org>
2016-07-27 13:03:58 +0200

    Print asserting lane configurations

--- include/util/debug.hpp
+++ include/util/debug.hpp
@@ -17,42 +17,42 @@ namespace util
 {
 namespace guidance
 {
-inline void print(const engine::guidance::RouteStep &step)
-{
-    std::cout << static_cast<int>(step.maneuver.instruction.type) << " "
-              << static_cast<int>(step.maneuver.instruction.direction_modifier) << "  "
-              << static_cast<int>(step.maneuver.waypoint_type) << " "
-              << " Lanes: (" << static_cast<int>(step.maneuver.lanes.lanes_in_turn) << ", "
-              << static_cast<int>(step.maneuver.lanes.first_lane_from_the_right) << ")"
-              << " Duration: " << step.duration << " Distance: " << step.distance
-              << " Geometry: " << step.geometry_begin << " " << step.geometry_end
-              << " exit: " << step.maneuver.exit << " Intersections: " << step.intersections.size()
-              << " [";
+//inline void print(const engine::guidance::RouteStep &step)
+//{
+//    std::cout << static_cast<int>(step.maneuver.instruction.type) << " "
+//              << static_cast<int>(step.maneuver.instruction.direction_modifier) << "  "
+//              << static_cast<int>(step.maneuver.waypoint_type) << " "
+//              << " Lanes: (" << static_cast<int>(step.maneuver.lanes.lanes_in_turn) << ", "
+//              << static_cast<int>(step.maneuver.lanes.first_lane_from_the_right) << ")"
+//              << " Duration: " << step.duration << " Distance: " << step.distance
+//              << " Geometry: " << step.geometry_begin << " " << step.geometry_end
+//              << " exit: " << step.maneuver.exit << " Intersections: " << step.intersections.size()
+//              << " [";
+//
+//    for (const auto &intersection : step.intersections)
+//    {
+//        std::cout << "(bearings:";
+//        for (auto bearing : intersection.bearings)
+//            std::cout << " " << bearing;
+//        std::cout << ", entry: ";
+//        for (auto entry : intersection.entry)
+//            std::cout << " " << (entry ? "true" : "false");
+//        std::cout << ")";
+//    }
+//    std::cout << "] name[" << step.name_id << "]: " << step.name;
+//}
 
-    for (const auto &intersection : step.intersections)
-    {
-        std::cout << "(bearings:";
-        for (auto bearing : intersection.bearings)
-            std::cout << " " << bearing;
-        std::cout << ", entry: ";
-        for (auto entry : intersection.entry)
-            std::cout << " " << (entry ? "true" : "false");
-        std::cout << ")";
-    }
-    std::cout << "] name[" << step.name_id << "]: " << step.name;
-}
-
-inline void print(const std::vector<engine::guidance::RouteStep> &steps)
-{
-    std::cout << "Path\n";
-    int segment = 0;
-    for (const auto &step : steps)
-    {
-        std::cout << "\t[" << segment++ << "]: ";
-        print(step);
-        std::cout << std::endl;
-    }
-}
+//inline void print(const std::vector<engine::guidance::RouteStep> &steps)
+//{
+//    std::cout << "Path\n";
+//    int segment = 0;
+//    for (const auto &step : steps)
+//    {
+//        std::cout << "\t[" << segment++ << "]: ";
+//        print(step);
+//        std::cout << std::endl;
+//    }
+//}
 
 inline void print(const extractor::guidance::Intersection &intersection)
 {
--- src/extractor/guidance/turn_lane_augmentation.cpp
+++ src/extractor/guidance/turn_lane_augmentation.cpp
@@ -1,11 +1,13 @@
 #include "extractor/guidance/turn_lane_augmentation.hpp"
 #include "extractor/guidance/turn_lane_types.hpp"
 #include "util/simple_logger.hpp"
+#include "util/debug.hpp"
 
 #include <algorithm>
 #include <boost/assert.hpp>
 #include <cstddef>
 #include <utility>
+#include <iostream>
 
 namespace osrm
 {
@@ -283,7 +285,10 @@ LaneDataVector handleNoneValueAtSimpleTurn(LaneDataVector lane_data,
         // a pgerequisite is simple turns. Larger differences should not end up here
         // an additional line at the side is only reasonable if it is targeting public
         // service vehicles. Otherwise, we should not have it
-        BOOST_ASSERT(connection_count + 1 == lane_data.size());
+        if (connection_count + 1 != lane_data.size()) {
+          std::cout << ">>> " << connection_count << ", " << lane_data.size() << std::endl;
+          util::guidance::print(lane_data);
+        }
 
         lane_data = mergeNoneTag(none_index, std::move(lane_data));
     }
